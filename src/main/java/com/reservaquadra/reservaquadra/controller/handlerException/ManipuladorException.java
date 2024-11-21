fe30fd7a787fd4e18be73bc4e010f2194f90f085
RicardoBarrote <ricardoambscfc@gmail.com>
2024-11-21 16:36:29 -0300

    Tratando excecoes com o controlador universal

--- src/main/java/com/reservaquadra/reservaquadra/controller/handlerException/ManipuladorException.java
+++ src/main/java/com/reservaquadra/reservaquadra/controller/handlerException/ManipuladorException.java
@@ -1,6 +1,9 @@
 package com.reservaquadra.reservaquadra.controller.handlerException;
 
 import com.reservaquadra.reservaquadra.exception.EntidadeNaoEncontradaException;
+import com.reservaquadra.reservaquadra.exception.StatusDeJogadorNaoExisteException;
+import com.reservaquadra.reservaquadra.exception.UsuarioContemUmaEquipeException;
+import com.reservaquadra.reservaquadra.exception.UsuarioJaEstaNaEquipeException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,30 @@ public class ManipuladorException {
     public ResponseEntity<PadraoDeErro> entidadeNaoEncontradaExceptionResponseEntity(EntidadeNaoEncontradaException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         String erro = "Entidade não encontrada.";
+        PadraoDeErro padraoDeErro = new PadraoDeErro(LocalDateTime.now(), status.value(), erro, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(padraoDeErro);
+    }
+
+    @ExceptionHandler(StatusDeJogadorNaoExisteException.class)
+    public ResponseEntity<PadraoDeErro> statusDeJogadorNaoExisteException(StatusDeJogadorNaoExisteException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String erro = "Verifique o formato da requisição, o status informado não existe.";
+        PadraoDeErro padraoDeErro = new PadraoDeErro(LocalDateTime.now(), status.value(), erro, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(padraoDeErro);
+    }
+
+    @ExceptionHandler(UsuarioContemUmaEquipeException.class)
+    public ResponseEntity<PadraoDeErro> usuarioContemUmaEquipeException(UsuarioContemUmaEquipeException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.CONFLICT;
+        String erro = "Usuário já está registrado em uma equipe.";
+        PadraoDeErro padraoDeErro = new PadraoDeErro(LocalDateTime.now(), status.value(), erro, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(padraoDeErro);
+    }
+
+    @ExceptionHandler(UsuarioJaEstaNaEquipeException.class)
+    public ResponseEntity<PadraoDeErro> usuarioJaEstaNaEquipeException(UsuarioJaEstaNaEquipeException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String erro = "Usuário já está cadastrado na equipe que você deseja.";
         PadraoDeErro padraoDeErro = new PadraoDeErro(LocalDateTime.now(), status.value(), erro, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(padraoDeErro);
     }
